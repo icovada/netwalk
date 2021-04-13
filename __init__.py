cb93f9189300546bfb420a3739350836a7b29671
Federico Tabbò (Europe) <federico.tabbo@global.ntt>
2021-04-13 23:57:36 +0200

    Import from new files

--- __init__.py
+++ __init__.py
@@ -1,5 +1,9 @@
-from .objects import Interface, Switch, Fabric
+"Main file for library"
+
 import logging
+from .interface import Interface
+from .switch import Switch
+from .fabric import Fabric
 
 __all__ = ["Interface", "Switch", "Fabric"]
 
