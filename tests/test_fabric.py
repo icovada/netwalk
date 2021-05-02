e31ee016f380f603db98316c0590a421ac2e2893
Federico Tabbò (Europe) <federico.tabbo@global.ntt>
2021-05-02 18:20:42 +0200

    Test global mac table

--- tests/test_fabric.py
+++ tests/test_fabric.py
@@ -276,5 +276,112 @@ class TestFabricBase(unittest.TestCase):
         paths = f.find_paths(c, [a, b])
         assert e.interfaces['GigabitEthernet0/2'] not in paths[0]
 
+    def test_mac_table(self):
+        from netaddr import EUI
+        """
+        A G0/0 --- G0/0 B
+      G0/1             G0/1
+        |               |
+      G0/1             G0/1
+        C G0/0 --- G0/0 D G0/2 - - PC
+        Check PC mac is on D-Gi0/2
+        """
+
+        f = Fabric()
+        a = Switch(hostname="A", facts={'hostname': 'A', 'fqdn': 'A.not set'})
+        b = Switch(hostname="B", facts={'hostname': 'B', 'fqdn': 'B.not set'})
+        c = Switch(hostname="C", facts={'hostname': 'C', 'fqdn': 'C.not set'})
+        d = Switch(hostname="D", facts={'hostname': 'D', 'fqdn': 'D.not set'})
+
+        f.switches = {'A': a,
+                      'B': b,
+                      'C': c,
+                      'D': d}
+
+        a00 = Interface(name='GigabitEthernet0/0',
+                        neighbors=[{'hostname': 'B',
+                                   'remote_int': 'GigabitEthernet0/0'}],
+                        switch=a)
+        a01 = Interface(name='GigabitEthernet0/1',
+                        neighbors=[{'hostname': 'C',
+                                    'remote_int': 'GigabitEthernet0/1'}],
+                        switch=a)
+        b00 = Interface(name='GigabitEthernet0/0',
+                        neighbors=[{'hostname': 'A',
+                                    'remote_int': 'GigabitEthernet0/0'}],
+                        switch=b)
+        b01 = Interface(name='GigabitEthernet0/1',
+                        neighbors=[{'hostname': 'D',
+                                    'remote_int': 'GigabitEthernet0/1'}],
+                        switch=b)
+        c00 = Interface(name='GigabitEthernet0/0',
+                        neighbors=[{'hostname': 'D',
+                                    'remote_int': 'GigabitEthernet0/0'}],
+                        switch=c)
+        c01 = Interface(name='GigabitEthernet0/1',
+                        neighbors=[{'hostname': 'A',
+                                    'remote_int': 'GigabitEthernet0/1'}],
+                        switch=c)
+        c02 = Interface(name='GigabitEthernet0/2',
+                        switch=c)
+        d00 = Interface(name='GigabitEthernet0/0',
+                        neighbors=[{'hostname': 'C',
+                                    'remote_int': 'GigabitEthernet0/0'}],
+                        switch=d)
+        d01 = Interface(name='GigabitEthernet0/1',
+                        neighbors=[{'hostname': 'B',
+                                    'remote_int': 'GigabitEthernet0/1'}],
+                        switch=d)
+
+        a.interfaces = {'GigabitEthernet0/0': a00,
+                        'GigabitEthernet0/1': a01}
+
+        b.interfaces = {'GigabitEthernet0/0': b00,
+                        'GigabitEthernet0/1': b01}
+
+        c.interfaces = {'GigabitEthernet0/0': c00,
+                        'GigabitEthernet0/1': c01,
+                        'GigabitEthernet0/2': c02}
+
+        d.interfaces = {'GigabitEthernet0/0': d00,
+                        'GigabitEthernet0/1': d01}
+
+        amac = EUI("aa:aa:aa:aa:aa:aa")
+        bmac = EUI("bb:bb:bb:bb:bb:bb")
+        cmac = EUI("cc:cc:cc:cc:cc:cc")
+        dmac = EUI("dd:dd:dd:dd:dd:dd")
+        pcmac = EUI("01:01:01:01:01:01")
+
+        a.mac_table = {amac: {'interface': a.interfaces['GigabitEthernet0/0']},
+                       cmac: {'interface': b.interfaces['GigabitEthernet0/0']},
+                       pcmac: {'interface': b.interfaces['GigabitEthernet0/0']},
+                       dmac: {'interface': b.interfaces['GigabitEthernet0/1']}}
+
+        b.mac_table = {amac: {'interface': b.interfaces['GigabitEthernet0/0']},
+                       cmac: {'interface': b.interfaces['GigabitEthernet0/1']},
+                       pcmac: {'interface': b.interfaces['GigabitEthernet0/1']},
+                       dmac: {'interface': b.interfaces['GigabitEthernet0/0']}}
+
+        c.mac_table = {amac: {'interface': c.interfaces['GigabitEthernet0/1']},
+                       bmac: {'interface': c.interfaces['GigabitEthernet0/1']},
+                       pcmac: {'interface': c.interfaces['GigabitEthernet0/2']},
+                       dmac: {'interface': c.interfaces['GigabitEthernet0/1']}}
+
+        d.mac_table = {amac: {'interface': d.interfaces['GigabitEthernet0/1']},
+                       bmac: {'interface': d.interfaces['GigabitEthernet0/1']},
+                       pcmac: {'interface': d.interfaces['GigabitEthernet0/1']},
+                       cmac: {'interface': d.interfaces['GigabitEthernet0/1']}}
+
+        for sw in [a,b,c,d]:
+            for _, data in sw.mac_table.items():
+                try:
+                    data['interface'].mac_count += 1
+                except KeyError:
+                    pass
+        f.refresh_global_information()
+
+        assert f.mac_table[pcmac] == {'interface' : c.interfaces['GigabitEthernet0/2']}
+
+
 if __name__ == '__main__':
     unittest.main()
