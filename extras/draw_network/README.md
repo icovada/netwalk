3c96d28439438213de8b1bf652ae2e2fa36aeca8
Federico Tabbò (Europe) <federico.tabbo@global.ntt>
2021-05-03 19:20:43 +0200

    add draw_network extra script

---
+++ extras/draw_network/README.md
@@ -0,0 +1,2 @@
+Script to generate a graph from a Fabric object.
+Example imports a pickled Fabric object stored in fabric_data.bin
---
+++ extras/draw_network/draw_network.py
@@ -0,0 +1,39 @@
+import pickle
+import networkx as nx
+from pyvis.network import Network
+from netwalk.netwalk import Interface
+
+#matplotlib.use("Agg")
+
+with open('fabric_data.bin', 'rb') as fabricfile:
+    fabric = pickle.load(fabricfile)
+
+g = nx.Graph()
+
+g.add_nodes_from([swdata.facts['fqdn'] for swname, swdata in fabric.switches.items()])
+
+labeldict = {y: x for x, y in fabric.switches.items()}
+
+for swname, swdata in fabric.switches.items():
+    for intname, intdata in swdata.interfaces.items():
+        if hasattr(intdata, 'neighbors'):
+            if len(intdata.neighbors) == 1:
+                if isinstance(intdata.neighbors[0], Interface):
+                    side_a = intdata.switch.facts['fqdn']
+                    side_b = intdata.neighbors[0].switch.facts['fqdn']
+
+                    #try:
+                    #    assert side_a.name != "SMba32_CStellaICT"
+                    #    assert side_b.name != "SMba32_CStellaICT"
+                    #except AssertionError:
+                    #    continue
+
+                    g.add_edge(side_a, side_b)
+
+
+
+
+nt = Network('100%', '75%')
+nt.show_buttons()
+nt.from_nx(g)
+nt.show('nx.html')
