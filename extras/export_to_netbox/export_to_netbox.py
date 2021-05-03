27e13a40a30a374702f19dd5fe37842737f1afb3
Federico Tabbò (Europe) <federico.tabbo@global.ntt>
2021-05-03 20:39:28 +0200

    Add ip addresses, prefixes and vlans

--- extras/export_to_netbox/export_to_netbox.py
+++ extras/export_to_netbox/export_to_netbox.py
@@ -2,20 +2,21 @@ import pickle
 import pynetbox
 import netwalk
 from slugify import slugify
+import logging
+import ipaddress
 
-def main():
-    with open('fabric_data.bin', 'rb') as fabricfile:
-        fabric = pickle.load(fabricfile)
+logger = logging.getLogger(__name__)
+logger.setLevel(level=logging.DEBUG)
 
-    nb = pynetbox.api(
-        'http://localhost',
-        token='d25885197f8a04fabf92aea757fb9f7a896f0b6c'
-    )
+nb = pynetbox.api(
+    'http://localhost',
+    token='d25885197f8a04fabf92aea757fb9f7a896f0b6c'
+)
 
-    nb_role = nb.dcim.device_roles.get(name="Access Switch")
-    nb_site = nb.dcim.sites.get(name="San Martino Buon Albergo")
-
+def create_devices_and_interfaces(fabric):
+    # Create devices and interfaces
     for swname, swdata in fabric.switches.items():
+        logging.info("Switch %s", swname)
         nb_device_type = nb.dcim.device_types.get(model=swdata.facts['model'])
         if nb_device_type is None:
             nb_manufacturer = nb.dcim.manufacturers.get(name=swdata.facts['vendor'])
@@ -36,6 +37,7 @@ def main():
                                                serial_number=swdata.facts['serial_number'])
 
         for interface in swdata.facts['interface_list']:
+            logger.info("Interface %s on switch %s", interface, swname)
             if "Fast" in interface:
                 int_type = "100base-tx"
             elif "Ten" in interface:
@@ -55,6 +57,51 @@ def main():
                                                          name=interface,
                                                          type=int_type)
 
+def add_ip_addresses(fabric):
+    for swname, swdata in fabric.switches.items():
+        nb_device = nb.dcim.devices.get(name=swdata.facts['hostname'])
+        for intname, intdata in swdata.interfaces_ip.items():
+            nb_interface = nb.dcim.interfaces.get(device_id=nb_device.id,
+                                                  name=intname)
+
+            for protocol, addresses in intdata.items():
+                for address, properties in addresses.items():
+                    ip = ipaddress.ip_interface(f"{address}/{properties['prefix_length']}")
+                    nb_prefix = nb.ipam.prefixes.get(prefix=str(ip.network),
+                                                     site_id=nb_site.id)
+                    logger.info("Checking prefix %s", str(ip.network))
+                    if nb_prefix is None:
+                        nb_prefix = nb.ipam.prefixes.create(prefix=str(ip.network),
+                                                            site=nb_site.id)
+
+                    nb_address = nb.ipam.ip_addresses.get(address=str(ip),
+                                                          site_id=nb_site.id)
+                    logger.info("Checking IP %s", str(ip))
+                    if nb_address is None:
+                        nb_address = nb.ipam.ip_addresses.create(address=str(ip),
+                                                                 site=nb_site.id)
+
+def add_l2_vlans(fabric):
+    for swname, swdata in fabric.switches.items():
+        for vlanid, vlandata in swdata.vlans.items():
+            nb_vlan = nb.ipam.vlans.get(vid=vlanid,
+                                        site_id=nb_site.id)
+            if nb_vlan is None:
+                nb_vlan = nb.ipam.vlans.create(vid=vlanid,
+                                               name=vlandata['name'],
+                                               site=nb_site.id)
+    pass
+
+def main():
+    #create_devices_and_interfaces(fabric)
+    #add_ip_addresses(fabric)
+    add_l2_vlans(fabric)
+
 
 if __name__ == '__main__':
+    with open('fabric_data.bin', 'rb') as fabricfile:
+        fabric = pickle.load(fabricfile)
+
+    nb_role = nb.dcim.device_roles.get(name="Access Switch")
+    nb_site = nb.dcim.sites.get(name="San Martino Buon Albergo")
     main()
