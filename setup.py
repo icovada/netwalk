c61052f718610312616dbb2c448861a1447d43e1
Federico Tabbò (Europe) <federico.tabbo@global.ntt>
2021-05-10 15:31:46 +0200

    V1.1.1 release

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name = "netwalk",
-    version = "1.1.0",
+    version = "1.1.1",
     author = "Federico Tabbò (Europe)",
     author_email = "federico.tabbo@global.ntt",
     description = "Network discovery and analysis tool",
