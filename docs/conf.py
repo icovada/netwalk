e21d4a2c151edb101de1bc3f9d10ac506abbd282
Federico Tabbò (Europe) <federico.tabbo@global.ntt>
2021-10-26 11:26:36 +0200

    Make it play nice with readthedocs, hopefully

--- docs/conf.py
+++ docs/conf.py
@@ -10,10 +10,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import os
-import sys
-sys.path.insert(0, os.path.abspath('../..'))
-import netwalk
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
 
 
 # -- Project information -----------------------------------------------------
@@ -35,14 +34,13 @@ extensions = [
     'sphinx.ext.autodoc'
 ]
 
-
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
 # -- Options for HTML output -------------------------------------------------
--- docs/index.rst
+++ docs/index.rst
@@ -1,5 +1,5 @@
 .. Netwalk documentation master file, created by
-   sphinx-quickstart on Tue Oct 26 09:47:58 2021.
+   sphinx-quickstart on Tue Oct 26 11:24:15 2021.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
