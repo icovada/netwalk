4fe6fc83fc474a705ff5f47eeb8d01de1425c827
Federico Tabbò (Europe) <federico.tabbo@global.ntt>
2021-11-19 17:34:56 +0100

    Test correct Port-channel assignment

--- tests/test_interface.py
+++ tests/test_interface.py
@@ -500,7 +500,6 @@ class TestL3Interface(unittest.TestCase):
         assert str(interface) == config    
 
 class TestPortChannel(unittest.TestCase):
-    import ipaddress
     def test_base_po(self):
         config = ("interface Ethernet0\n"
                   " channel-group 1 mode active\n"
@@ -516,10 +515,11 @@ class TestPortChannel(unittest.TestCase):
         
         po = netwalk.Interface(config=config)
 
-
         switch = netwalk.Switch(hostname="testswitch")
         switch.add_interface(interface)
         switch.add_interface(po)
+
+        interface.parse_config()
 
         assert interface.parent_interface==po
         assert interface in po.child_interfaces
