eca40a516c1e024dbe5e7c0d0a3b22b948a254c7
Federico Tabbò (Europe) <federico.tabbo@global.ntt>
2021-11-22 12:48:54 +0100

    Add interface to object

--- netwalk/objects.py
+++ netwalk/objects.py
@@ -1,7 +1,7 @@
 "contains root objects"
 
 from typing import Dict
-from netwalk.interface import Interface
+from netwalk.interface import Interface, Switch
 import logging
 
 
@@ -14,3 +14,16 @@ class Device():
         self.logger = logging.getLogger(__name__ + hostname)
         self.hostname: str = hostname
         self.interfaces: Dict[str, 'Interface'] = {}
+
+    def add_interface(self, intobject: Interface):
+        """Add interface to device
+
+        :param intobject: Interface to add
+        :type intobject: netwalk.Interface
+        """
+        intobject.switch = self
+        self.interfaces[intobject.name] = intobject
+
+        if type(self) == Switch:
+            for k, v in self.interfaces.items():
+                v.parse_config()
