f1ce426ac8e22cb341265e41184f5a7ff1fa51b8
Federico Tabbò (Europe) <federico.tabbo@global.ntt>
2022-04-04 22:05:33 +0200

    version 1.6

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name = "netwalk",
-    version = "1.5",
+    version = "1.6",
     author = "Federico Tabbò (Europe)",
     author_email = "federico.tabbo@global.ntt",
     description = "Network discovery and analysis tool",
